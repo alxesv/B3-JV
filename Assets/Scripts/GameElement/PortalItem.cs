5dbabff1a00c9447bab43e5fcd3c067ebb2c3375
Mizer3 <91741197+Mizer3@users.noreply.github.com>
2024-11-14 13:37:48 +0100

    portal ok avec son

---
+++ Assets/Scripts/GameElement/PortalItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PortalItem : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetLocation;
+
+
+    public AudioSource portalAudioSource;
+
+    public void Start()
+    {
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = targetLocation.position;
+            portalAudioSource.Play();
+            Destroy(gameObject);
+        }
+    }
+}
