3ab3508ba5609d2c0ec90e2582c422242496c134
kvong92 <vongkevin.c92@gmail.com>
2024-11-15 00:03:09 +0100

    add:notification-nbr-bonus

--- Assets/Scripts/GameElement/SaltItem.cs
+++ Assets/Scripts/GameElement/SaltItem.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SaltItem : MonoBehaviour
 {
     public AudioSource saltAudioSource;
+    public static int saltPoint = 50;
+    public static bool isBonusSalt = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        saltAudioSource.Play();
         if (collision.gameObject.CompareTag("Player"))
         {
-            GameManager.score += 50;
-            Destroy(gameObject);
+            saltAudioSource.Play();
+            isBonusSalt = true;
+            GameManager.score += saltPoint;
+            Destroy(gameObject, saltAudioSource.clip.length);
         }
     }
 }
